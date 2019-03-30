08f9589ae9e0a01341705c5409d20097f29b5be9
Marcus Lindstrom <mali1741@gmail.com>
2019-03-30 17:53:24 +0100

    added nunchuck support

--- keyboard.ino
+++ keyboard.ino
@@ -1,4 +1,32 @@
+#include <NintendoExtensionCtrl.h>
+#include "keys.h"
+
+void getKey(){
+  boolean success = nchuk.update();  // Get new data from the controller
+
+  thiskey = 0;
+  
+  if (nchuk.joyY() > 192) {
+    thiskey |= KEY_UP; 
+  } else if (nchuk.joyY() < 64) {
+    thiskey |= KEY_DOWN;     
+  }
+  if (nchuk.joyX() > 192) {
+    thiskey |= KEY_RIGHT; 
+  } else if (nchuk.joyX() < 64) {
+    thiskey |= KEY_LEFT;     
+  }
+  if (nchuk.buttonC()) thiskey |= KEY_A; 
+
+  if (nchuk.buttonZ()) thiskey |= KEY_B; 
+
+  // KEY_SELECT and KEY_START omitted 
+}
+
+
+/*
 #include <Wire.h>
+
 
 void geti2cAdress(){
   byte error,address;
@@ -40,3 +68,4 @@ void getKey(){
   if((dio_in & 1) == 0)
     thiskey |= 128; 
 }
+*/
